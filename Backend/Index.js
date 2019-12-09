6104cf6bc80058f61059e40c593f9c80b12fa214
dev-indra <indrawanjonmuhamad@yahoo.com>
2019-12-09 20:19:11 +0700

    Fixed MongoClient Connection, but get 'undefined'

--- Backend/Index.js
+++ Backend/Index.js
@@ -6,17 +6,19 @@ const cors = require('cors');
 
 const Mongo = require('./Models/Connect');
 
-const mongo = new Mongo();
+const mongo = new Mongo("mongodb://localhost:27017/pemilo");
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 app.use(bp.json());
 app.use(cors());
 
 app.listen(4000, (err)=>{
-    
+
     if(err){console.log("Error")}
-    
-    else{console.log("Server [status:running]");}
+
+    else{
+        console.log("Server [status:running]");
+    }
 
     console.log(mongo.checkConnection());
 
--- Backend/Models/Connect.js
+++ Backend/Models/Connect.js
@@ -3,12 +3,28 @@ const ID = require('mongodb').ObjectID;
 
 class MongoDB{
 
-    constructor(url){this.url = url;}
+    constructor(url){
+        this.url = url;
+    }
 
     checkConnection(){
-        Mongo.connect(this.url, {useNewUrlParser:true, useUnifiedTopology:true}, (err)=>{
-            if(err){return `Err ${err}`}
-            else{ return "Database [status:'running']" }
+        /*Mongo.connect("mongodb://localhost:27017/pemilo", {useNewUrlParser:true, useUnifiedTopology:true}, (err)=>{
+            if(err){
+                console.log(err);
+                return `Err ${err}`
+            }
+            else{
+                return "Database [status:'running']"
+            }
+        });*/
+
+        Mongo.connect(this.url, {useNewUrlParser:true, useUnifiedTopology:true} , function(err, db) {
+            if(err){
+                console.log("Error : ", err);
+                return `Err ${err}`
+            }else{
+                return console.log("Database [status:'running']");
+            }
         });
     }
 
