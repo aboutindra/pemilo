e8e7075ed6c51a8c1c765d2a2add9ebd63bdc27b
dev-indra <indrawanjonmuhamad@yahoo.com>
2020-01-03 21:04:00 +0700

    Changes nyangkut wkwkw

--- Frontend/Web/src/Component/Dashboard/Home/Center.jsx
+++ Frontend/Web/src/Component/Dashboard/Home/Center.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './../../../Resource/App.css';
 
 export default function CenterHome() {
     return (
--- Frontend/Web/src/Component/Dashboard/Home/Left.jsx
+++ Frontend/Web/src/Component/Dashboard/Home/Left.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './../../../Resource/App.css';
 
 export default function LeftHome() {
     return (
         <div>
-
+            
         </div>
     );
 }
