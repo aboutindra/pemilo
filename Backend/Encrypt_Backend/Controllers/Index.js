dd53541a06f48773e1342cb0978a306d19ea12ba
dev-indra <indrawanjonmuhamad@yahoo.com>
2020-02-04 23:01:24 +0700

    Add Encrypt Backend

---
+++ Backend/Encrypt_Backend/Controllers/Index.js
@@ -0,0 +1,6 @@
+
+class ControllerIndex {
+
+
+
+}
--- Backend/Encrypt_Backend/Index.js
+++ Backend/Encrypt_Backend/Index.js
@@ -1,27 +1,26 @@
 
+const bp = require('body-parser');
 const cors = require('cors');
 const compress = require('compression');
 
 const express = require('express');
 const app = new express();
 
-const route = require('./Route/Index');
-const Route = new route();
+const route = require('./Routes/Index');
 
 app.use(compress());
 app.use(cors());
+app.use(bp.json());
 
-app.use("/en", Route);
+app.use("/", route);
 
 app.listen(4004, async (err) => {
-
     if(err) { console.log("Encrypt failed to running, error : ", err) }
 
-    app.post("*", Route.ntfnd);
-    app.delete("*", Route.ntfnd);
-    app.update("*", Route.ntfnd);
-    app.put("*", Route.ntfnd);
+    app.post("*", (req,res) => { res.send({ status : "OK" }) });
+    app.delete("*", (req,res) => { res.send({ status : "OK" }) });
+    app.get("*", (req,res) => { res.send({ status : "OK" }) });
+    app.put("*", (req,res) => { res.send({ status : "OK" }) });
 
     console.log("Server http://localhost:4004/ [Encrypt:running]");
-
 });
